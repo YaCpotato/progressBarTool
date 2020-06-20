dd7052ee53121d82cf2d10e51de272e92c9c901a
YaCpotato <yasshisshy@gmail.com>
2020-06-21 01:08:35 +0900

    だいたいOK。あとは個別SVG出力機能

--- app.js
+++ app.js
@@ -4,55 +4,57 @@ var app = new Vue({
       return {
         stg: "hello",
         svg: null,
+        totalSlideNum:0,
         bar: {
           x: 10,
           y: 10,
           rx: 20,
           ry: 10,
-          width: 200,
-          height: 50,
-          lineColor:"#776677",
-          strokeColor:"#545454",
+          width: 500,
+          height: 25,
+          lineColor:"#000000",
+          strokeColor:"#00FF00",
           strokeWidth:5
         }
       };
     },
     methods: {
       appendBar() {
-        $("rect").remove();
+        $("#box_1").empty();
         var svg_elem = document.getElementById("box_1");
-        d3Element = d3.select("#box_1")
+        let d3Element = d3.select("#box_1")
+        for(let i = 0;i<this.totalSlideNum;i++){
+            var backBar = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+            backBar.setAttribute("x", this.bar.x);
+            backBar.setAttribute("y", this.bar.y + (i * (this.bar.height + 10)) + 10);
+            backBar.setAttribute("stroke", this.bar.strokeColor);
+            backBar.setAttribute("stroke-width", this.bar.strokeWidth);
+            backBar.setAttribute("ry", this.bar.ry);
+            backBar.setAttribute("width", this.bar.width);
+            backBar.setAttribute("height", this.bar.height);
+            backBar.setAttribute("fill", this.bar.lineColor);
+            backBar.setAttribute("style","stroke-opacity: .3;fill-opacity: .3")
+            svg_elem.appendChild(backBar);
 
-        var backBar = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-        backBar.setAttribute("x", this.bar.x);
-        backBar.setAttribute("y", this.bar.y);
-        backBar.setAttribute("stroke", this.bar.strokeColor);
-        backBar.setAttribute("stroke-width", this.bar.strokeWidth);
-        backBar.setAttribute("ry", this.bar.ry);
-        backBar.setAttribute("width", 500);
-        backBar.setAttribute("height", this.bar.height);
-        backBar.setAttribute("fill", this.bar.lineColor);
-        backBar.setAttribute("style","stroke-opacity: .3;fill-opacity: .3")
-        svg_elem.appendChild(backBar);
+            var progress = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+            progress.setAttribute("id", "progressBar-"+i);
+            progress.setAttribute("x", this.bar.x);
+            progress.setAttribute("y", this.bar.y + (i * (this.bar.height + 10)) + 10);
+            progress.setAttribute("stroke", this.bar.strokeColor);
+            progress.setAttribute("stroke-width", this.bar.strokeWidth);
+            progress.setAttribute("ry", this.bar.ry);
+            progress.setAttribute("width", (this.bar.width / this.totalSlideNum) * (i + 1));
+            progress.setAttribute("height", this.bar.height);
+            progress.setAttribute("fill", this.bar.lineColor);
+            svg_elem.appendChild(progress);
 
-        var progress = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-        progress.setAttribute("id", "progressBar");
-        progress.setAttribute("x", this.bar.x);
-        progress.setAttribute("y", this.bar.y);
-        progress.setAttribute("stroke", this.bar.strokeColor);
-        progress.setAttribute("stroke-width", this.bar.strokeWidth);
-        progress.setAttribute("ry", this.bar.ry);
-        progress.setAttribute("width", this.bar.width);
-        progress.setAttribute("height", this.bar.height);
-        progress.setAttribute("fill", this.bar.lineColor);
-        svg_elem.appendChild(progress);
-
-        d3Element.append("text")
-        .attr("x", ($("#progressBar").width() / 2) + 10)
-        .attr("y", ($("#progressBar").height() / 2) + 15)
-        .text("20")
-        .attr("fill",this.bar.strokeColor)
-        .style("text-anchor", "middle")
+            d3Element.append("text")
+            .attr("x", ($("#progressBar-"+i).width() / 2) + 10)
+            .attr("y", ($("#progressBar-"+i).height() / 2) + 15  + (i * (this.bar.height + 10)) + 10)
+            .text(parseInt((100 / this.totalSlideNum)*(i + 1)) + "%")
+            .attr("fill",this.bar.strokeColor)
+            .style("text-anchor", "middle")
+          }
       }
     },
     mounted() {
